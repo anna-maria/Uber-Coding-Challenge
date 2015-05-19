cc3f7c0097c5d9dba3061613b85d9206e081585f
Anna Gyergyai <agyergyai@gmail.com>
2015-05-19 19:19:10 -0400

    changed test to index

--- api/index.php
+++ api/index.php
@@ -2,8 +2,8 @@
 	require '../vendor/autoload.php';
 
 	$app = new \Slim\Slim();
-	$app->get('/hello/:name', function ($name) {
-	    echo "Hello, $name";
+	$app->get('/', function () {
+	    echo "Hello";
 	});
 	$app->run();
 
