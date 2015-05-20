7e39c2b17ed4a592e3e05d08c7aa68ac8559bc0e
Anna Gyergyai <agyergyai@gmail.com>
2015-05-19 20:27:33 -0400

    added database access

--- api/index.php
+++ api/index.php
@@ -1,25 +1,58 @@
 <?php
-	require '../vendor/autoload.php';
+    require '../vendor/autoload.php';
 
-	$app = new \Slim\Slim();
-	$app->get('/', function () {
-	    echo "Hello";
-	});
-	$app->run();
+    $app = new \Slim\Slim();
 
-	/*getMovies();
+    $app->get('/map', function () {
+        echo '<html>
+              <head>
+                <style type="text/css">
+                  html, body, #map-canvas { height: 100%; margin: 0; padding: 0;}
+                </style>
+                <script type="text/javascript"
+                  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCzZsdcUsJuf7DhjDhu4vhyOIiiQeAQ-2A">
+                </script>
+                <script type="text/javascript">
+                  function initialize() {
+                    var mapOptions = {
+                      center: { lat: 37.783, lng: -122.417},
+                      zoom: 12
+                    };
+                    var map = new google.maps.Map(document.getElementById(\'map-canvas\'),
+                        mapOptions);
+                    /*var myLatlng = new google.maps.LatLng(37.819,-122.479);
+                    var marker = new google.maps.Marker({
+                          position: myLatlng,
+                          map: map,
+                          title: \'Golden Gate Bridge!\'
+                      });*/
+                  }
+                  google.maps.event.addDomListener(window, \'load\', initialize);
+                </script>
+              </head>
+              <body>
+                <div style="margin-top:10px;margin-bottom:10px;text-align:center;">To Start, Search By Movie Title: <input type="text" name="title" value=""></div>
+                <div id="map-canvas"></div>
+              </body>
+            </html>';
+    });
 
-	function getMovies() {
-		$error = '';
-		//$reply = curlIt('https://data.sfgov.org/resource/yitu-d5am.json', $error); 
-		$reply = curlIt('https://data.sfgov.org/resource/wwmu-gmzc.json', $error);
-		if ($reply == false) {
-			// curl error
-		} else {
-			var_dump(json_decode($reply));
-		}
-	}
-	
+    $app->get('/movie', function () {
+        echo getMovies();
+    });
+    $app->run();
+
+    function getMovies() {
+        $error = '';
+        $reply = curlIt('https://data.sfgov.org/resource/yitu-d5am.json', $error); 
+        //$reply = curlIt('https://data.sfgov.org/resource/wwmu-gmzc.json', $error);
+        if ($reply == false) {
+            // curl error
+        } else {
+            return $reply;
+        }
+    }
+    
     function curlIt($url, &$error) {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -37,33 +70,7 @@
             $error = curl_error($ch);
             return false;
         } else {
-        	return $returned;
+            return $returned;
         }
         curl_close ($ch);
-
-        /*$openTag = '<result>';
-        $closeTag = '</result>';
-        $pos1 = strpos($returned,$openTag);
-        $pos2 = strpos($returned,$closeTag);
-        if ($pos1 && $pos2) {
-            $pos1+=strlen($openTag);
-            $response = substr($returned,$pos1,$pos2-$pos1);
-        }
-
-        if ($returned=='Received' || (isset($response) && $response=='success') || $response=='queued') {
-            return true;
-        }
-        else {
-            $reason = 'unknown';
-            
-            $pos1 = strpos($returned,'<reason>');
-            $pos2 = strpos($returned,'</reason>');
-            if ($pos1 && $pos2) {
-                $pos1+=strlen('<reason>');
-                $reason = substr($returned,$pos1,$pos2-$pos1);
-            }
-
-            $error = $reason;
-            return false;
-        }*/
-    //}
+    }
